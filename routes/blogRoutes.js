b7a6b145a43f0d0a7713b797fe6bd417b438da1c
YoProgrammer <yuttasakcom@gmail.com>
2018-03-16 00:53:44 +0700

    move clearHash service to minddleware

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
-const { clearHash } = require('../services/cache')
+const clearHash = require('../middlewares/clearCache')
 
 const Blog = mongoose.model('Blog')
 
@@ -23,7 +23,7 @@ module.exports = app => {
     res.send(blogs)
   })
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, clearHash, async (req, res) => {
     const { title, content } = req.body
 
     const blog = new Blog({
@@ -38,7 +38,5 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err)
     }
-
-    clearHash(req.user.id)
   })
 }
