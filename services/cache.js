1428c0015276390a84071acc2289f2ad2b9a4f2c
YoProgrammer <yuttasakcom@gmail.com>
2018-03-16 09:19:39 +0700

    create cache service

--- services/cache.js
+++ services/cache.js
@@ -10,6 +10,7 @@ client.hget = util.promisify(client.hget)
 mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true
   this.hashKey = JSON.stringify(options.key || '')
+
   return this
 }
 
@@ -17,6 +18,7 @@ mongoose.Query.prototype.exec = async function () {
   if (!this.useCache) {
     return exec.apply(this, arguments)
   }
+
   const key = JSON.stringify(Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name
   }))
@@ -32,6 +34,7 @@ mongoose.Query.prototype.exec = async function () {
 
   const result = await exec.apply(this, arguments)
   client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10)
+
   return result
 }
 
